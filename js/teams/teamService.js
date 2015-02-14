fa5e0f9de3b3e791e344819b81b8d6a3f302dd43
lrobins <lrobins@adobe.com>
2015-02-14 13:09:08 -0700

    initial commit with a few methods added to teamService

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,51 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		// if (parseInt(gameObj.homeTeamScore) || parseInt(gameObj.opponentScore) === NaN) {
+		// 	alert("those aren't numbers, stupid head")
+		// 	break;
+		// }
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won === true;
+		} else { 
+			gameObj.won === false;
+		}
+		$http(
+		{
+			method: 'POST',
+			url: url,
+			data: gameObj
+		}).then(function() {
+			//
+		})
+	}; // end of addNewGame
+
+	this.getTeamData = function(team) {
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http(
+		{
+			method: 'GET',
+			url: url,
+			//data: ??
+		}).then(function(data) {
+			deferred.resolve(data);
+			//
+		})
+		return deferred.promise;
+	}
+
+
 });
+
+
+
+
+
+
+
+
+
+
