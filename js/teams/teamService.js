6ec5ae9405e7169223af7d0208ccd3d1cfe12b03
lrobins <lrobins@adobe.com>
2015-02-14 13:18:30 -0700

    this completes the TeamService.js

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -32,8 +32,20 @@ app.service('teamService', function($http, $q){
 			url: url,
 			//data: ??
 		}).then(function(data) {
-			deferred.resolve(data);
-			//
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++) {
+				if(results[i].won === true) {
+					wins++;
+				} else {
+					wins--;
+				}
+			};
+			results.wins = wins; //adding a property to results array
+			results.losses = losses;
+			deferred.resolve(results);
+			
 		})
 		return deferred.promise;
 	}
@@ -42,7 +54,17 @@ app.service('teamService', function($http, $q){
 });
 
 
-
+// Now that we have complete wins and losses variables, we need to somehow access those variables outside of our service. 
+// We know that we have a results array which holds an array of all the games the particular team has played. 
+// What if we do something a little unconventional here. We know we're going to eventually resolve our promise we made 
+// earlier with the results variable (so we can access all the games in our controller). 
+// We also know that an array is really just an object at heart. Let's add a 'wins' property to the results array 
+// and set it equal to our wins variable and let's also set a 'losses' property on our results array and set it equal 
+// to our losses variable. I know this is a little weird because we're not adding items to our array like we usually do 
+// but instead we're adding properties to this array. It's a good reminder that arrays are just objects. 
+// Once you add the wins and losses property, go ahead and resolve our deferred object we made earlier with our results array.
+// * Now that we've set up those two methods on our teamService object, we can close teamService. We won't need to modify this 
+// file again but we will need to call the methods we set up in teamService.js later.
 
 
 
