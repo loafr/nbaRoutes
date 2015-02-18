0ab8ea631d3284c015b0f61e8c7543dbb5ce548d
lrobins <lrobins@adobe.com>
2015-02-17 18:40:18 -0700

    adds a few initial changes (i.e. wrong syntax) before work starts today

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,20 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function() {
+		$scope.showNewGameForm != $scope.showNewGameForm;
+	};
+	
+
+
+	$scope.getTeamData = function($route.current.params.team) { // not sure about this yet...
+		//
+	};
+
+
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -18,9 +18,7 @@ app.service('teamService', function($http, $q){
 			method: 'POST',
 			url: url,
 			data: gameObj
-		}).then(function() {
-			//
-		})
+		});
 	}; // end of addNewGame
 
 	this.getTeamData = function(team) {
@@ -53,18 +51,6 @@ app.service('teamService', function($http, $q){
 
 });
 
-
-// Now that we have complete wins and losses variables, we need to somehow access those variables outside of our service. 
-// We know that we have a results array which holds an array of all the games the particular team has played. 
-// What if we do something a little unconventional here. We know we're going to eventually resolve our promise we made 
-// earlier with the results variable (so we can access all the games in our controller). 
-// We also know that an array is really just an object at heart. Let's add a 'wins' property to the results array 
-// and set it equal to our wins variable and let's also set a 'losses' property on our results array and set it equal 
-// to our losses variable. I know this is a little weird because we're not adding items to our array like we usually do 
-// but instead we're adding properties to this array. It's a good reminder that arrays are just objects. 
-// Once you add the wins and losses property, go ahead and resolve our deferred object we made earlier with our results array.
-// * Now that we've set up those two methods on our teamService object, we can close teamService. We won't need to modify this 
-// file again but we will need to call the methods we set up in teamService.js later.
 
 
 
